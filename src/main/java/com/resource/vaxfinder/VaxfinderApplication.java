9d1a1fd10e61c167509c837d394e1e3b8d1dea31
Japneet Kaur Saluja <japneetkaursaluja@Japneets-MacBook-Air.local>
2021-05-29 13:38:30 +0530

    initial commit

---
+++ src/main/java/com/resource/vaxfinder/VaxfinderApplication.java
@@ -0,0 +1,13 @@
+package com.resource.vaxfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaxfinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaxfinderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/resource/vaxfinder/VaxfinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.resource.vaxfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaxfinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
